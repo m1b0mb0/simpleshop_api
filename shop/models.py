7f8f366b9829a5d4fbfec02b9c9e5778a4b7354b
Illia <illaredka8@gmail.com>
2026-02-10 20:11:16 +0200

    added shop app with category and product models

---
+++ shop/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.utils.text import slugify
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    image = models.ImageField(upload_to='categories/', blank=True, null=True)
+    
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+class Product(models.Model):
+    categoty = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    slug = models.SlugField(max_length=255, unique=True, blank=True, null=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, null=True)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
