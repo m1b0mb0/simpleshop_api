8e2f9d07ff44461d220e4948a8304c078e4395f3
Illia <illaredka8@gmail.com>
2026-02-13 17:49:26 +0200

    add cart app with Cart and CartItem models

---
+++ cart/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.conf import settings
+from shop.models import Product
+
+class Cart(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart of {self.user.username}"
+    
+    @property
+    def total_cost(self):
+        return sum(item.cost for item in self.items.all())
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} - {self.product}"
+    
+    @property
+    def cost(self):
+        return self.product.price * self.quantity
--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'users',
     'shop',
     'rest_framework',
+    'cart',
 ]
 
 MIDDLEWARE = [
